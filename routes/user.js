83b6f50b5499fec86355a7f8253e0291027bbd73
Mahendra1452 <86612795+Mahendra1452@users.noreply.github.com>
2023-09-18 18:45:24 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,7 @@ import { User } from "../models/user.js";
 import mongoose from "mongoose";
 import { getAllUsers,
     register,
-    specialFunc,
-    getUserByID,
     login,
-    updateUser,
-    deleteUser,
     getMyProfile,
     logout} 
     from "../controller/user.js";
@@ -15,20 +11,6 @@ import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-mongoose
-    .connect("mongodb+srv://mahendragudla:keil754A_2456@cluster0.0nxlmbb.mongodb.net/",{
-        dbName: "backend_api",
-    })
-    .then(()=>console.log("Database connected"))
-    .catch(e=>console.log(e))
-
-
-// /userid/rfeferf
-// /userid/abhi
-// this has to be above the /userid/:id (dynamic routing)
-// dynamic routing supposed to be at last
-
-router.get("/userid/special",specialFunc)
 
 router.get('/all', getAllUsers);
 
@@ -37,8 +19,6 @@ router.post("/new", register);
 router.post("/login", login)
 
 router.get("/logout", logout)
-
-router.route("/userid/:id").get(getUserByID).put(updateUser).delete(deleteUser);
 
 router.get("/me",isAuthenticated,getMyProfile);
 
